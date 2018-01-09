7b5eb25be04babf34afe159cd86f5aa31c99bbfc
andrastemesi <35278259+andrastemesi@users.noreply.github.com>
2018-01-10 00:41:23 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Im Andras Temesi and you see the first c++ code that i uploaded.:)";
+    return 0;
+}
